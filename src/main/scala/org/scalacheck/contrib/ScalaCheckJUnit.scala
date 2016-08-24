e7d13828f2f2f15650a7b40dcfc6fb3c0121d679
Fabian Krämer <fakraemer@atlassian.com>
2016-08-24 11:40:32 +1000

    Preserve original stack trace if property evaluation failed

--- src/main/scala/org/scalacheck/contrib/ScalaCheckJUnit.scala
+++ src/main/scala/org/scalacheck/contrib/ScalaCheckJUnit.scala
@@ -41,7 +41,8 @@ class ScalaCheckJUnitPropertiesRunner(suiteClass: java.lang.Class[Properties]) e
 				case Test.Passed => {} // Test passed, nothing to do
 				case Test.Proved(_) => {} // Test passed, nothing to do
 				case Test.Exhausted => notifier.fireTestIgnored(desc) // exhausted tests are marked as ignored in JUnit
-				case _ => notifier.fireTestFailure(new Failure(desc, new Throwable(Pretty.pretty(res)))) // everything else is also a failed test
+				case Test.PropException(_, exception, _) => notifier.fireTestFailure(new Failure(desc, exception)) // property evaluation escaped
+ 				case _ => notifier.fireTestFailure(new Failure(desc, new Throwable(Pretty.pretty(res)))) // everything else is also a failed test
 			}
 		}
 	}
